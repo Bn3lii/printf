0ac36483c94584d0de53ba9036e3f6f3231401b9
Bn 3li <134309374+Bn3lii@users.noreply.github.com>
2024-01-31 03:05:43 +0200

    Create string.c

---
+++ string.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * handle_string - Handles the %s format
+ * @args: va_list argument containing the string to print
+ * @len: Pointer to length counter
+ *
+ * Return: updated lenth counter
+ */
+int handle_string(va_list args, int *len)
+{
+	const char *str = va_arg(args, const char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str != '\0')
+	{
+		*len += _putchar(*str++);
+	}
+	return (*len);
+}
